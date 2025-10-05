cc3cec77b1f1d0b9d214848a011ba54ee8bf4d3a
JGomez1612 <jostgomez1612@gmail.com>
2025-10-05 16:12:14 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState, useReducer } from 'react'
+import './App.css'
+
+// Components
+
+function App() {
+ 
+
+
+  return (
+    <>
+      <h1>To Do List</h1>
+    </>
+  )
+}
+
+export default App
